12e466776a2fe73e3869e5f58c2513bc6ec8acab
Ken <ken+git@teapla.net>
2013-07-14 00:46:38 +0900

    35まで作成。

---
+++ 35.go
@@ -0,0 +1,19 @@
+package main
+
+import "code.google.com/p/go-tour/pic"
+
+func Pic(dx, dy int) [][]uint8 {
+	img := make([][]uint8, dy)
+
+	for y := range img {
+		img[y] = make([]uint8, dx)
+		for x := range img[y] {
+			img[y][x] = uint8((x + y) / 2)
+		}
+	}
+	return img
+}
+
+func main() {
+	pic.Show(Pic)
+}
